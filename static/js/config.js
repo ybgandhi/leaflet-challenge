80329db8404bfd12acc7395d7aebae83d6c15723
ybgandhi <gandhibyash@gmail.com>
2021-06-15 18:06:29 -0400

    update first step js file

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoieWJnOTQiLCJhIjoiY2tvenU2djN3MDkyejJva2c1NHVuamxqdCJ9.2t9sQoMFl79-nnQU_z6eQA";
+const API_KEY = "pk.eyJ1IjoieWJnOTQiLCJhIjoiY2tweWt6cnk5MDU0bTJxbW53dTFrZnRnaSJ9.f9mSOUgeaKsef28uruRijQ";
--- static/js/logic.js
+++ static/js/logic.js
@@ -71,4 +71,19 @@ d3.json(eqURL, function(eqData){
     }).addTo(eq);
     // send earthquake layer to create Map function
     eq.addTo(myMap);
-})
+
+    // add legend
+    var legend = L.control({position: "bottomright"});
+    legend.onAdd = function() {
+        var div = L.DomUntil.create("div", "info legend"),
+        depth = [-10, 10, 30, 50, 70, 90];
+
+        div.innerHTML += "<h3 style='text-align: center'>Depth</h3>"
+    for (var i =0; i < depth.length; i++){
+        div.innerHTML +=
+        '<i style="background:' + chooseColor(depth[i] + 1) + '"></i> ' + depth[i] + (depth[i+1] ? '&ndash;' +depth[i+1]+ '<br>': '+');
+        }
+        return div;
+    };
+    legend.addTo(myMap);
+});
